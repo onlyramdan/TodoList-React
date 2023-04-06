5b1590bbc1e1a8603fc126105bc922366a4cbfce
onlyramdan <onlyramdan@apps.ipb.ac.id>
2023-04-07 02:33:48 +0700

    Semuanya Aman Beres

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,34 @@
 import React from "react";
 
-export const TodoItem = ({ todo }) => {
+export const TodoItem = ({ todo, setRefresh }) => {
+  const updateTodo = () => {
+    todo.done = !todo.done;
+
+    fetch("http://localhost:8000/todos/" + todo.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    }).then(() => {
+      console.log("todo updated");
+      setRefresh(true);
+    });
+  };
+  const deleteTodo = () => {
+    fetch("http://localhost:8000/todos/" + todo.id, {
+      method: "DELETE",
+    }).then(() => {
+      console.log("todo deleted");
+      setRefresh(true);
+    });
+  };
   return (
     <li className={`${todo.done ? "checked" : ""}`}>
-      {todo.title}
-      <span class="close">x</span>
+      <div onClick={updateTodo}>{todo.title}</div>
+      <span class="close" onClick={deleteTodo}>
+        x
+      </span>
     </li>
   );
 };
